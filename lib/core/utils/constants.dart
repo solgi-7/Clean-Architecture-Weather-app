14f79136b0e17e7048879821ee1e4c03888922fe
solgi-7 <116170412+solgi-7@users.noreply.github.com>
2023-01-11 15:06:04 +0330

    dio and api call

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants{
+  static const baseUrl = "https://home.openweathermap.org";
+  static const apiKeys1 = "62127d69ad86cdc3231034a49b4e2448";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_weather_app/core/main_wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,11 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp();
+    return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainWrapper(),
+    );
   }
 }
 
